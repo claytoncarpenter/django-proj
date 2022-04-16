c2913fe24be74672ab47e20dbd4d234ad2d16827
claytoncarpenter <claytonwc672@gmail.com>
2022-04-16 09:03:57 -0500

    created model sensor

--- mysite/firstapp/models.py
+++ mysite/firstapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class sensorData(models.Model):
+    datetime = models.DateTimeField(auto_now_add = True )
+    temp = models.DecimalField(max_digits=3, decimal_places=2)
+    hum = models.DecimalField(max_digits=3, decimal_places=2)
