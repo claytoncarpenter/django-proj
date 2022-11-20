22663babafc75d2c2b63cbc97190f8323fe07f0b
claytoncarpenter <claytonwc672@gmail.com>
2022-11-20 17:25:56 -0600

    added crispyt forms

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'firstapp',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # added from tutorial on forms
